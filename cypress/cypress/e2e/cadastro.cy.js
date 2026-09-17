43e3679dc062644ee43eb4fbbecab6724285b2f0
Marcia Ferreira <mmarcia_ferreira@yahoo.com.br>
2026-09-17 18:47:43 -0300

    Automação da Lista de Catalógo

--- cypress/cypress/e2e/cadastro.cy.js
+++ cypress/cypress/e2e/cadastro.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress/">
+import { faker } from '@faker-js/faker';
 
 describe('Funcionalidade Cadastro no Hub e Leitura', () => {
 
@@ -6,16 +7,31 @@ describe('Funcionalidade Cadastro no Hub e Leitura', () => {
     cy.visit('register.html')
   });
 
-  it('Deve fazer cadastro com sucesso', () => {
+  it('Deve fazer cadastro com sucesso usando função JS', () => {
     let email = `teste${Date.now()}@teste.com.br`
-    
-    cy.get('#name').type('Marcia')
+
+    cy.get('#name').type("marcia")
     cy.get('#email').type(email)
     cy.get('#phone').type('11983766655')
     cy.get('#password').type('teste@1234')
     cy.get('#confirm-password').type('teste@1234')
     cy.get('#terms-agreement').check()
     cy.get('#register-btn').click()
+
+  })
+
+  it('Deve fazer cadastro com sucesso usando Faker', () => {
+    let nome = faker.person.fullName()
+    let email = faker.internet.email()
+
+    cy.get('#name').type(nome)
+    cy.get('#email').type(email)
+    cy.get('#phone').type('11983766655')
+    cy.get('#password').type('teste@1234')
+    cy.get('#confirm-password').type('teste@1234')
+    cy.get('#terms-agreement').check()
+    cy.get('#register-btn').click()
+    cy.get('#user-name').should('contain', nome)
 
   })
 })
